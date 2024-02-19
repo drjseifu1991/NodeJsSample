01746fd0d66e8d1afba4d5fd54c79be1a8a84c47
Dereje Seifu <Derejeseifu3030@gmail.com>
2024-02-19 23:59:08 +0300

    updated

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const PORT = 5000;
 let books = [
   { id: 1, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
   { id: 2, title: '1984', author: 'George Orwell' },
-  { id: 3, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' }
+  { id: 3, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+  { id: 4, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+  { id: 5, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+  { id: 6, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' }
 ];
 
 // Middleware to parse JSON body
